a978e7d3876c186e7e4c0be8153aedeb91886df7
minemobs <issahakim94@gmail.com>
2023-06-25 11:06:27 +0200

    Fixed mobile version

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -6,12 +6,14 @@
 </script>
 
 <div class="max-w-xs rounded-md bg-[#24273a]">
-	<img height="48" class="object-cover overflow-hidden rounded-md mt-3 w-full h-48" src={imageSrc} alt="Project" />
-	<div class="px-6 py-4">
+	<div class="mt-3 pl-2 pr-2 md:flex-shrink-0 shadow">
+		<img class="object-cover border-none w-full h-full rounded-lg rounded-b-none" src={imageSrc} alt="Project" />
+	</div>
+	<div class="max-sm:px-4 md:px-6 pt-3">
 		<h4 class="mb-3 text-xl font-semibold tracking-tight text-sky-300">{title}</h4>
 		<p class="mb-2 leading-normal text-white">{description}</p>
 		<a href={projectURL}>
-			<div class="m-5 p-3 bg-slate-700 rounded-md flex flex-1 justify-center">
+			<div class="max-sm:m-2 md:m-5 py-3 bg-slate-700 rounded-md flex flex-1 justify-center">
 				<button class="text-white">Github Repo</button>
 			</div>
 		</a>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Card from '../Card.svelte';
-	import japDict from '../../lib/images/japdict.png';
+	import japDictImg from '../../lib/images/japdict.png';
+	import websiteImg from '../../lib/images/website.png';
 </script>
 
 <svelte:head>
@@ -8,15 +9,15 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="flex flex-1 justify-center items-center gap-20">
+<div class="flex flex-1 justify-center items-center max-sm:gap-10 md:gap-20 mx-5">
 	<Card
-		imageSrc={japDict}
+		imageSrc={japDictImg}
 		title="JapDict"
-		description="A japanese dictionary made in Vala with GTK 4 and Libadwaita"
+		description="A japanese dictionary made in Vala with GTK 4 and LibAdwaita"
 		projectURL="https://github.com/minemobs/japdict"
 	/>
 	<Card
-		imageSrc={japDict}
+		imageSrc={websiteImg}
 		title="This website"
 		description="A small portfolio of my current projects"
 		projectURL="https://github.com/minemobs/minemobs.github.io"
